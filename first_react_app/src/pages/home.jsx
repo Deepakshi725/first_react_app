60ad9ed7e49ce0cef6041aa49614259b44024526
Deepakshi275 <sharmapanchi99@gmail.com>
2025-02-07 13:23:23 +0530

    Created a react app

---
+++ first_react_app/src/pages/home.jsx
@@ -0,0 +1,15 @@
+//eslint-disable-next-line
+import React from "react";
+import "../App.css";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Deepakshi</h1>
+      <p>Contact: 70185xxxx3</p>
+      <p>Email:deepakshi.sharma@kalvium.community</p>
+    </div>
+  );
+};
+
+export default Home;
